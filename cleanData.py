3d1e2c156697c4fede3ec21ce0b59ded01105a26
Walks-with-Drongos <benjaminmurphy2406@gmail.com>
2025-04-23 01:23:54 +0200

    visits column

--- cleanData.py
+++ cleanData.py
@@ -1,21 +1,63 @@
 #print("Hello World")
-filename = "weights_2025030105.txt"
-print(filename)
+#filename = "weights_2025030105.txt"
+#print(filename)
 
-f = open(filename, "r")
-lines = f.read().splitlines()
+#f = open(filename, "r")
+#lines = f.read().splitlines()
 #print(f.readlines())
 
-for line in lines:
-    x = line.split(",")
-    print(x)
+# for line in lines:
+#     x = line.split(",")
+#     print(x)
 
 # next step column headers
-# identifying which rows to delete (1 second or weight threshold)
+# identifying which rows to delete (1 second or weight threshold) - SOLVED MAX
 
-print(x)
+# print(x)
 
-#import pandas as pd
-#df = pd.DataFrame(x)
-#df.columns = ['TIME', 'DATE', 'OBS', 'WEIGHT','STATUS']
-#print(df)
+import pandas as pd
+df = pd.read_csv('GA1105h00_01032025.csv')
+# print(df)  #BEFORE
+
+df['Visit'] = None
+Visit = 0
+for index, row in df.iterrows():
+ if row['OBS.NO.'] == 1: Visit = Visit +1 
+ df.at[index, 'Visit'] = Visit #row['Age'] + 5 
+
+print(df) #AFTER
+df.to_csv('Ga1105h00_01032025_Visit.csv', index=False)
+
+df2 = pd.read_csv('GA1105h00_01032025_Visit.csv')
+print(df2['Visit'].dtype)
+
+grouped_visit = df2.groupby('Visit')['WEIGHT'].max()
+print(grouped_visit)
+# for i, rowi in grouped_visit.items():
+# #  print(i,rowi)
+# # print(type(grouped_visit))
+
+test_df = pd.DataFrame(columns=['DATE', 'TIME', 'WEIGHT', 'VISIT'])
+test_df.loc[1] = ['foo','bar','baz','quux']
+print(test_df)
+
+#df['Visit'] = df['Visit'].astype('string')
+#df['WEIGHT'] = df['WEIGHT'].astype(float)
+
+max_weight_indices = df2.groupby('Visit')['WEIGHT'].idxmax()
+print(max_weight_indices)
+#Then, use .loc to get the corresponding rows
+max_weight_rows = df2.loc[max_weight_indices].reset_index(drop=True)
+# # This will retain TIME, DATE, OBS.NO, WEIGHT, and VISIT for the max weight row of each VISIT group
+print(max_weight_rows)
+
+# # # grouped_visit['DATE'] = None
+# # # grouped_visit['TIME'] = None
+# # for i, rowi in grouped_visit.iterrows():
+# #   for j, rowj in df.iterrows():
+# #     if rowi['WEIGHT'] == rowj['WEIGHT'] and rowi['Visit'] == rowj['Visit']:
+# #      grouped_visit.at[j, 'DATE'] = rowj['DATE']
+# #      grouped_visit.at[j, 'TIME'] = rowj['TIME']
+# #  # for Visit in grouped_visit
+# #  print("GroupedbyVisit:\n", grouped_visit)
+
